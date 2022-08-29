83e868e73aa22ee4045ba69e0c7bc022601a1ea4
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2022-08-30 02:03:26 +0300

    deleted component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,68 @@
 <template>
   <div id="app">
-    <HomePage />
+    <div class="container">
+      <div class="title">
+        <h1>1 le 100 arasında bir sayı tutacağım , tahmin et</h1>
+      </div>
+
+      <div class="input">
+        <p>Tuttum Tahmin Et</p>
+        <input
+          v-model="b"
+          id="girdi"
+          placeholder="Tahmin et"
+          type="number"
+          min="1"
+          max="100"
+        />
+      </div>
+      <div class="submit">
+        <button @click="fonksiyon">Onayla</button>
+      </div>
+
+      <div id="result">{{ result }}</div>
+      <br />
+      <div class="attempt">Tüm Denemelerin : {{ attempts }}</div>
+      <br />
+      <div v-if="show" class="numbofattempts">
+        {{ numbOfAttempts }} Denemede Bildin
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HomePage from "./components/HomePage.vue";
-
 export default {
   name: "App",
-  components: {
-    HomePage,
+  components: {},
+  data: function () {
+    return {
+      result: "",
+      a: Math.floor(Math.random() * 101),
+      b: document.getElementById("girdi").value,
+      attempts: [],
+      numbOfAttempts: "",
+      show: false,
+    };
+  },
+  methods: {
+    fonksiyon: function () {
+      if (this.b > 100) {
+        window.alert("100'den küçük bir değer giriniz");
+      }
+      if (this.a == this.b) {
+        this.result = "Bildin Helal Olsun!";
+        this.numbOfAttempts = this.attempts.length;
+        this.show = true;
+      }
+      if (this.b < this.a) {
+        this.result = "Bilemedin Yukarı Çık";
+      }
+      if (this.b > this.a) {
+        this.result = "Bilemedin Aşağı İn";
+      }
+      this.attempts.push(document.getElementById("girdi").value);
+    },
   },
 };
 </script>
@@ -23,5 +75,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: aqua;
+}
+
+button {
+  width: 200px;
+  height: 50px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: large;
+  text-align: center;
+  background-color: antiquewhite;
+  color: red;
+}
+.input {
+  margin-top: 50px;
+}
+#girdi {
+  width: 100px;
+  height: 25px;
+}
+.submit {
+  margin-top: 40px;
 }
 </style>
